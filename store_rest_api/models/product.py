e87fcfbb9611abde4eb0e849cafae53411281781
chengchinlim <chengchin5227@gmail.com>
2023-09-04 13:01:25 -0500

    alternate many to many direction in store and product models

--- store_rest_api/models/product.py
+++ store_rest_api/models/product.py
@@ -1,8 +1,6 @@
 from store_rest_api.models.base import BaseModel
 from django.db import models
 
-from store_rest_api.models.store import Store
-
 
 class Product(BaseModel):
     class Meta:
@@ -10,4 +8,3 @@ class Product(BaseModel):
         app_label = 'store_rest_api'
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
-    stores = models.ManyToManyField(Store, related_name='+', null=False)
--- store_rest_api/models/store.py
+++ store_rest_api/models/store.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 
+from store_rest_api.models import Product
 from store_rest_api.models.base import BaseModel
 from django.db import models
 
@@ -10,4 +11,5 @@ class Store(BaseModel):
         app_label = 'store_rest_api'
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE, null=True)
+    products = models.ManyToManyField(Product, related_name='+')
 
